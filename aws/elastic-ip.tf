93cc3c82220345b7dcbde946294b107c3e4e3ef3
bob-crutchley <bob.crutchley96@gmail.com>
2019-07-25 08:07:18 +0100

    example now has ssh connection

--- aws/elastic-ip.tf
+++
@@ -1,4 +0,0 @@
-resource "aws_eip" "default" {
-  instance = "${aws_instance.default.id}"
-  vpc = true
-}
--- aws/instance.tf
+++ aws/instance.tf
@@ -4,6 +4,19 @@ resource "aws_instance" "default" {
   instance_type = "t2.micro"
   subnet_id = "${aws_subnet.default.id}"
   vpc_security_group_ids = ["${aws_security_group.default.id}"]
+  associate_public_ip_address = true
   key_name = "default-key-pair"
+  provisioner "remote-exec" {
+    connection {
+      type = "ssh"
+      host = "${self.public_ip}"
+      user = "ubuntu"
+      private_key = "${file("~/.ssh/id_rsa")}"
+    }
+    inline = [
+      "echo hi",
+      "hostname"
+    ]
+  }
 }
 
